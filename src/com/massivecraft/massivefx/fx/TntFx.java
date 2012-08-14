cb45596470a504230e0099b4ea64ea62cd64e959
Olof Larsson <olof@sylt.nu>
2012-08-14 17:20:40 +0200

    Update for minecraft 1.3.1

--- src/com/massivecraft/massivefx/fx/TntFx.java
+++ src/com/massivecraft/massivefx/fx/TntFx.java
@@ -1,18 +1,10 @@
 package com.massivecraft.massivefx.fx;
 
-import java.util.HashSet;
 import java.util.Map;
 
-import net.minecraft.server.ChunkPosition;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.Packet60Explosion;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.craftbukkit.CraftWorld;
 
-import com.massivecraft.massivefx.Conf;
+import com.massivecraft.mcore3.util.SmokeUtil;
 
 public class TntFx extends BaseFx
 {
@@ -24,22 +16,9 @@ public class TntFx extends BaseFx
 	@Override
 	public void playAt(Location location, int data)
 	{
-		fakeExplosion(location);
+	    SmokeUtil.fakeExplosion(location);
 	}
 	
 	public static final TntFx instance = new TntFx("MASSIVEFX_TNT", true, true, null, "Non-damaging tnt explosion", "tnt", "tntexplosion", "explostion");
-	
-	// -------------------------------------------- //
-	// TNT PACKET UTILS
-	// -------------------------------------------- //
-	
-	public static void fakeExplosion(Location location)
-	{
-		HashSet<ChunkPosition> chunkPositions = new HashSet<ChunkPosition>();
-		Packet60Explosion packet = new Packet60Explosion(location.getX(),location.getY(), location.getZ(), 0.1f, chunkPositions);
-		CraftServer craftServer = (CraftServer) Bukkit.getServer();
-		MinecraftServer minecraftServer = craftServer.getServer();
-		minecraftServer.serverConfigurationManager.sendPacketNearby(location.getX(), location.getY(), location.getZ(), Conf.fxBlockViewDistanceUnlimited, ((CraftWorld)location.getWorld()).getHandle().dimension, packet);
-	}
 }
 
