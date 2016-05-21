9a5ba9e7c1b95c4f8fe930d3bf358f1969094fcd
Olof Larsson <olof@sylt.nu>
2016-05-21 14:49:24 +0200

    MassiveCore - PermissionUtil Improvements

--- src/com/massivecraft/massivefx/Permission.java
+++ src/com/massivecraft/massivefx/Permission.java
@@ -20,7 +20,7 @@ public enum Permission
 	
 	public boolean has(CommandSender sender, boolean informSenderIfNot)
 	{
-		return PermissionUtil.has(sender, this.node, informSenderIfNot);
+		return PermissionUtil.hasPermission(sender, this.node, informSenderIfNot);
 	}
 	
 	public boolean has(CommandSender sender)
